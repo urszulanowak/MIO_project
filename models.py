4e82154c60caad1b72ee37dd6a00a5aa0c7b6abd
slash122 <gleb.avrutin@gmail.com>
2024-06-10 10:59:22 +0200

    build models

--- models.py
+++ models.py
@@ -3,36 +3,28 @@ from keras import layers
 from keras import models
 import numpy as np
 
-def build_encoder(input_dim, encoding_dim):
-    input_layer = layers.Input(shape=(input_dim,))
 
-    encoded = layers.Dense(128, activation='relu')(input_layer)
-    encoded = layers.Dense(64, activation='relu')(encoded)
-    encoded = layers.Dense(encoding_dim, activation='relu')(encoded)
-    
-    encoder = models.Model(input_layer, encoded, name="encoder")
-    return encoder
-
-
-def build_decoder(encoding_dim, output_dim):
-    encoded_input = layers.Input(shape=(encoding_dim,))
-
-    decoded = layers.Dense(64, activation='relu')(encoded_input)
-    decoded = layers.Dense(128, activation='relu')(decoded)
-    decoded = layers.Dense(output_dim, activation='sigmoid')(decoded)
-    
-    decoder = models.Model(encoded_input, decoded, name="decoder")
-    return decoder
+def build_encoder(input_shape, encoding_layers):
+    input_layer = layers.Input(shape=input_shape)
+    x = input_layer
+    for units, activation in encoding_layers:
+        x = layers.Dense(units, activation=activation)(x)
+    model = models.Model(input_layer, x, name="encoder")
+    return model
 
 
-def build_autoencoder(input_dim, encoding_dim):
-    encoder = build_encoder(input_dim, encoding_dim)
-    decoder = build_decoder(encoding_dim, input_dim)
-    
+def build_decoder(encoding_dim, decoding_layers):
+    encoded_input = layers.Input(shape=encoding_dim)
+    x = encoded_input
+    for units, activation in decoding_layers:
+        x = layers.Dense(units, activation=activation)(x)
+    model = models.Model(encoded_input, x, name="decoder")
+    return model
+
+
+def build_autoencoder(input_dim, encoder, decoder):
     autoencoder_input = layers.Input(shape=(input_dim,))
-
     encoded = encoder(autoencoder_input)
     decoded = decoder(encoded)
-    
-    autoencoder = models.Model(autoencoder_input, decoded, name="autoencoder")
-    return autoencoder, encoder, decoder
+    autoencoder = models.Model(inputs=autoencoder_input, outputs=decoded, name="autoencoder")
+    return autoencoder
